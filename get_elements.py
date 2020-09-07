cf2b8a930ff3718e7fd710ae728add345918756d
SYSCOMWORLD\torstein.gombos <tg@syscomworld.com>
2020-09-07 09:41:29 +0200

    First commit. Will try to install numpy and pandas next.

---
+++ get_elements.py
@@ -0,0 +1,38 @@
+import requests as req
+import json
+import datetime
+import pandas as pd
+import time
+
+
+
+def api_call(url="https://fantasy.premierleague.com/api/bootstrap-static/"):
+    return req.get(url).json()
+
+
+def checkpoint_mark():
+
+    with open("checkpoint/API_call_timestamp.txt", "w") as f:
+        f.writelines(f"Timestamp: {datetime.date.today()} - {datetime.datetime.now().strftime('%H:%M:%S')}")
+
+
+def store_value_in_dataframe(json_data):
+    pd.DataFrame(data=json_data["elements"]).to_csv(f"historic_Data/{datetime.date.today()}.csv")
+
+
+if __name__ == '__main__':
+    # Add checkpoint for Splunk call
+    checkpoint_mark()
+
+    # Get data
+    data = api_call()
+
+    # Send data to LOG for Splunk
+    print(json.dumps(data["elements"]))
+
+    # Store value in history. Used later for trendlines
+    store_value_in_dataframe(data)
+
+
+
+
